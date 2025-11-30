e84e0aa11073f7b15ee3c437730a7b285ea82519
ccxpdtd <3045804265@qq.com>
2025-11-30 12:53:08 +0800

    update

---
+++ routes/favorites.js
@@ -0,0 +1,109 @@
+// routes/article.js
+const express = require('express')
+const router = express.Router()
+const db = require('../db') // 你自己的数据库连接模块
+
+// 上传文章（只有管理员可以上传）
+// router.post('/uploadArticle', (req, res) => {
+//   const { uname, title, description, author, content } = req.body;
+
+//   // 先查这个用户名的 role
+//   const checkUserSql = 'SELECT role FROM users WHERE username = ?';
+//   db.query(checkUserSql, [uname], (err, results) => {
+//     if (err) {
+//       console.error('查询用户权限失败:', err);
+//       return res.status(500).json({ code: 500, msg: '服务器错误' });
+//     }
+
+//     if (results.length === 0) {
+//       return res.status(403).json({ code: 201, msg: '用户不存在' });
+//     }
+
+//     const role = results[0].role;
+//     if (role !== 'admin') {
+//       return res.status(403).json({ code: 201, msg: '你没有权限发布文章' });
+//     }
+
+//     // 插入文章
+//     const insertSql = 'INSERT INTO articles (title,description,author,uname, content) VALUES (?, ?,?,?,?)';
+//     db.query(insertSql, [title, description, author, uname, content], (err2, result) => {
+//       if (err2) {
+//         console.error('插入文章失败:', err2);
+//         return res.status(500).json({ code: 200, msg: '文章上传失败' });
+//       }
+
+//       res.json({ code: 200, msg: '文章上传成功' });
+//     });
+//   });
+// });
+
+const handleFavorites = (results) => {
+  const video = results.filter(item => item.type === 'video')
+  const album = results.filter(item => item.type === 'album')
+  const show = results.filter(item => item.type === 'show')
+  const game = results.filter(item => item.type === 'game')
+  const recently_watching = results.filter(item => item.type === 'recently_watching')
+  const recently_listening = results.filter(item => item.type === 'recently_listening')
+  return {
+    video,
+    album,
+    show,
+    game,
+    recently_watching,
+    recently_listening
+  }
+}
+
+// 读取所有文章接口
+router.get('/get_myFavorites', (req, res) => {
+
+  const sql = `SELECT * FROM my_favorites 
+                ORDER BY created_at DESC;`;
+  db.query(sql, (err, results) => {
+    if (err) {
+      console.error('获取喜欢列表失败:', err);
+      return res.json({
+        code: 500,
+        msg: '获取喜欢列表失败'
+      });
+    }
+    const favorites = handleFavorites(results)
+    res.send({
+      code: 200,
+      msg: '获取喜欢列表成功',
+      favorites
+    });
+  });
+
+});
+
+// //删除指定文章
+// router.post('/delete_article/:id', (req, res) => {
+
+//   const id = req.params.id;
+//   const sql = 'DELETE FROM articles WHERE id = ?'
+//   db.query(sql, [id], (err, result) => {
+//     if (err) return res.status(500).send({ code: 500, msg: '删除文章失败' })
+//     if (result.affectedRows > 0) return res.send({ code: 200, msg: '删除文章成功' })
+//   })
+
+// })
+
+// //获取指定文章
+// router.get('/get_article/:id', (req, res) => {
+
+//   const id = req.params.id;
+
+//   const sql = 'select * from articles WHERE id = ?'
+
+//   db.query(sql, [id], (err, result) => {
+//     if (err) return res.status(500).send({ code: 500, msg: '获取文章失败' })
+//     return res.send({
+//       code: 200,
+//       msg: '获取文章成功',
+//       article: result[0]
+//     })
+//   })
+// })
+
+module.exports = router
